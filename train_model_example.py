5e64e5965e3094a871006cf75ffde8cc36a8b4f9
janos <risk.limits@gmail.com>
2019-07-02 11:01:55 -0400

    Added F1 score

--- train_model_example.py
+++ train_model_example.py
@@ -1,16 +1,21 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout, CuDNNLSTM, Masking, SimpleRNN
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Masking, SimpleRNN
 import numpy as np
 import h5py
+import pprint
+
+"""
+This does a a canonical LSTM model 
+
+The CuDNNLSTM is much faster but it does not support Masking
 
 """
 
-"""
 
+def main(input_file_name, target_name):
 
-def main(input_file_name):
+    target_index_name = "static_condition_condition_concept_name|" + target_name
 
     f5 = h5py.File(input_file_name)
 
@@ -18,33 +23,35 @@ def main(input_file_name):
     f5_target = f5["/data/processed/train/target/core_array"]
     f5_target_labels = f5["/data/processed/train/target/column_annotations"]
 
+    print("Dimensions of training set:")
+    print(f5_train.shape)
+    print("Dimension of full target:")
+    print(f5_target.shape)
+
     target_labels = f5_target_labels[...].ravel().tolist()
     target_labels = [str(s, "utf8") for s in target_labels]
 
+    # Get a count of the most frequent labels
     sum_targets = np.sum(f5_target[...], axis=0).tolist()
-
     sum_target_with_labels = [(target_labels[i].split("|")[-1], sum_targets[i]) for i in range(len(target_labels))]
-
     sum_target_with_labels.sort(key=lambda x: -1 * x[1])
 
-    print(sum_target_with_labels[0:50])
-
-    # raise(RuntimeError)
     # Let us generate a list of most frequent features to target for prediction
+    print("The top 25 variables in the test set")
+    pprint.pprint(sum_target_with_labels[0:25])
+    print("")
+    print("Predicting: '%s'" % target_name)
 
-    target_index = target_labels.index("static_condition_condition_concept_name|Acute renal failure syndrome")
-    # print(target_labels)
-    # target_index = target_labels.index("static_condition_condition_concept_name|Anxiety disorder")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Essential hypertension")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Type 2 diabetes mellitus without complication")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Respiratory failure")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Heart failure")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Chronic kidney disease")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Hyperkalemia")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Sepsis")
+    target_index = target_labels.index(target_index_name)
 
     f5_test = f5["/data/processed/test/sequence/core_array"]
     f5_test_target = f5["/data/processed/test/target/core_array"]
+
+
+    print("Dimensions of test set:")
+    print(f5_test.shape)
+    print("Dimensions of target:")
+    print(f5_test_target.shape)
 
     f5_train_array = f5_train[...]
     f5_train_array[np.isnan(f5_train_array)] = 0
@@ -55,22 +62,10 @@ def main(input_file_name):
     f5_test_array[np.isnan(f5_test_array)] = 0
 
     model = Sequential()
-    # model.add(SimpleRNN(128, activation="tanh", return_sequences=True))
-    # #model.add(Dropout(0.10))
-    #
-    # model.add(SimpleRNN(64, activation="tanh"))
-    # #model.add(Dropout(0.10))
-    #
-    # model.add(Dense(64, activation="sigmoid"))
-    # #model.add(Dropout(0.10))
-
-    #model.add(LSTM(128, activation="tanh", input_shape=(100,182)))
 
     model.add(Masking(mask_value=0.0, input_shape=f5_train_array.shape[1:]))
-
     model.add(LSTM(128, activation="tanh", return_sequences=True))
     model.add(Dropout(0.2))
-
     model.add(LSTM(128, activation="tanh"))
     model.add(Dropout(0.2))
     model.add(Dense(64, activation="relu"))
@@ -80,33 +75,79 @@ def main(input_file_name):
 
     opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
 
-    # configure sgd with gradient norm clipping
-    # opt = SGD(lr=0.01, momentum=0.9, clipnorm=1.0)
-
-    print(f5_train.shape[1:])
-
     model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
-
+    # This can be refactored as explicit casts are not needed
     model.fit(np.array(f5_train_array, dtype="float32"), np.array(f5_target[:, target_index], dtype="int32"), epochs=3,
               validation_data=(np.array(f5_test_array, dtype="float32"),
-                                                                                   np.array(f5_test_target[:,target_index],dtype="int32")))
+              np.array(f5_test_target[:,target_index],dtype="int32")))
+
+    model.save("coeffs.hdf5")
+    # Make a probability prediction
     y_pred_keras = model.predict_proba(np.array(f5_test[...], dtype="float32")).ravel()
 
-    # y_pred_keras = model.predict_proba(f5_test[...]).ravel()
+    print("Diagnostics of predictions")
+
+    show_first_n = 25
+    prediction_threshold = 0.5
+
+    print("Actual test predictions %s:" % show_first_n)
+    print(f5_test_target[0:show_first_n, target_index])
+
+    print("Probability of first %s predictions from the model:" % show_first_n)
+    print(y_pred_keras[0:show_first_n])
+
+    target_threshold_predictions = np.array(y_pred_keras > prediction_threshold, dtype="float32")
+    print("Setting the prediction threshold at %s" % prediction_threshold)
+    print(target_threshold_predictions[0:show_first_n])
+
+    print("Total positive cases in test set:")
+    print(np.sum(f5_test_target[:, target_index]))
+
+    print("Total predicted positive cases in test set:")
+    print(np.sum(target_threshold_predictions))
+
+    from sklearn.metrics import roc_curve, roc_auc_score, f1_score
+
+    model_auc_score = roc_auc_score(np.array(f5_test_target[:, target_index],dtype="int32"), y_pred_keras)
+    print("Computed AUC of the ROC:")
+    print(model_auc_score)
+
+    f1 = f1_score(np.array(target_threshold_predictions, dtype="int32"), np.array(f5_test_target[:, target_index], dtype="int32"))
+
+    print("F1 score:")
+    print(f1)
+
+
+
+
+
 
     # fpr_keras, tpr_keras, thresholds_keras = roc_curve(np.array(f5_test_target[:, target_index],dtype="int32"), y_pred_keras)
 
-    model.save("coeffs.hdf5")
-
-    print(y_pred_keras[0:60])
-    print(f5_test_target[0:60, target_index])
-    print(np.sum(f5_test_target[0:60, target_index]))
-    print(np.array(y_pred_keras[0:60] > 0.5, dtype="float32"))
-    print(np.sum(y_pred_keras[0:60]))
-
-    from sklearn.metrics import roc_curve, roc_auc_score
-    print(roc_auc_score(np.array(f5_test_target[:, target_index],dtype="int32"), y_pred_keras))
-
-
 if __name__ == "__main__":
-    main("Y:\\healthfacts\\ts\\processed_ohdsi_sequences.hdf5")
+    main("Y:\\healthfacts\\ts\\processed_ohdsi_sequences.hdf5", "Acute renal failure syndrome")
+
+    # Code attic
+
+    # print(target_labels)
+    # target_index = target_labels.index("static_condition_condition_concept_name|Anxiety disorder")
+    # target_index = target_labels.index("static_condition_condition_concept_name|Essential hypertension")
+    # target_index = target_labels.index("static_condition_condition_concept_name|Type 2 diabetes mellitus without complication")
+    # target_index = target_labels.index("static_condition_condition_concept_name|Respiratory failure")
+    # target_index = target_labels.index("static_condition_condition_concept_name|Heart failure")
+    # target_index = target_labels.index("static_condition_condition_concept_name|Chronic kidney disease")
+    # target_index = target_labels.index("static_condition_condition_concept_name|Hyperkalemia")
+    # target_index = target_labels.index("static_condition_condition_concept_name|Sepsis")
+
+
+    # configure sgd with gradient norm clipping
+    # from tensorflow.keras.optimizers import SGD
+    # opt = SGD(lr=0.01, momentum=0.9, clipnorm=1.0)
+
+    # model.add(SimpleRNN(128, activation="tanh", return_sequences=True))
+    # model.add(Dropout(0.10))
+    #
+    # model.add(Dense(64, activation="sigmoid"))
+    # #model.add(Dropout(0.10))
+
+    # model.add(LSTM(128, activation="tanh", input_shape=(100,182)))
