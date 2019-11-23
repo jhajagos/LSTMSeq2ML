ae98b614489c337632a5f2e86269814a55cadeae
janos <risk.limits@gmail.com>
2019-11-22 19:06:15 -0500

    Started to refactor this to make runtime information tracking

--- train_model_example.py
+++ train_model_example.py
@@ -6,6 +6,9 @@ import numpy as np
 import h5py
 import pprint
 import argparse
+import datetime
+import os
+
 
 """
 This does a a canonical LSTM model 
@@ -15,7 +18,8 @@ The CuDNNLSTM is much faster but it does not support Masking
 """
 
 
-def main(input_file_name, target_name, n_cut=25):
+def main(input_file_name, target_name, output_directory="./", n_cut=25, prediction_threshold=0.5, epochs=10, batch_size=100,
+         learning_rate=1e-3, learning_rate_decay=1e-4):
 
     target_index_name = "static_condition_condition_concept_name|" + target_name
 
@@ -64,6 +68,7 @@ def main(input_file_name, target_name, n_cut=25):
     f5_test_array = f5_test[...]
     f5_test_array[np.isnan(f5_test_array)] = 0
 
+    #TODO: Make models plugin
     model = Sequential()
     model.add(Masking(mask_value=0.0, input_shape=f5_train_array.shape[1:]))
     model.add(LSTM(256, activation="tanh", return_sequences=True))
@@ -74,30 +79,37 @@ def main(input_file_name, target_name, n_cut=25):
     model.add(Dropout(0.2))
 
     model.add(Dense(1, activation="sigmoid"))
-    opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-4)
+
+    opt = tf.keras.optimizers.Adam(lr=learning_rate, decay=learning_rate_decay)
 
     model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
+    best_model_file_name = os.path.join(output_directory, "best_model.h5")
+
     es_callback = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2, restore_best_weights=True)
-    mc_callback = ModelCheckpoint('best_model.h5', monitor='val_loss', mode='min', verbose=1, save_best_only=True)
+    mc_callback = ModelCheckpoint(best_model_file_name, monitor='val_loss', mode='min', verbose=1, save_best_only=True)
+
     # fit model
 
     # This can be refactored as explicit casts are not needed
-    model.fit(np.array(f5_train_array, dtype="float32"), np.array(f5_target[:, target_index], dtype="int32"), epochs=10,
+    model.fit(np.array(f5_train_array, dtype="float32"), np.array(f5_target[:, target_index], dtype="int32"), epochs,
               validation_data=(np.array(f5_test_array, dtype="float32"),
               np.array(f5_test_target[:, target_index], dtype="int32")), callbacks=[es_callback, mc_callback],
-              batch_size=100)
+              batch_size=batch_size)
 
-    model.load_weights("best_model.h5")
+    model.load_weights(best_model_file_name)
 
-    model.save(target_name_label + "_coeffs.hdf5")
+    time_stamp = datetime.date.utcnow().strftime("%Y%m%d_%H%M")
+
+    coefficients_filename = target_name_label + "_" + time_stamp + "_coeffs.hdf5"
+    model.save(os.path.joins(output_directory, coefficients_filename))
+
     # Make a probability prediction
     y_pred_keras = model.predict_proba(np.array(f5_test[...], dtype="float32")).ravel()
 
     print("Diagnostics of predictions")
 
     show_first_n = n_cut
-    prediction_threshold = 0.5
 
     print("Actual test predictions %s:" % show_first_n)
     print(f5_test_target[0:show_first_n, target_index])
@@ -120,7 +132,7 @@ def main(input_file_name, target_name, n_cut=25):
 
     from sklearn.metrics import roc_curve, roc_auc_score, f1_score, classification_report, average_precision_score
 
-    model_auc_score = roc_auc_score(np.array(f5_test_target[:, target_index],dtype="int32"), y_pred_keras)
+    model_auc_score = roc_auc_score(np.array(f5_test_target[:, target_index], dtype="int32"), y_pred_keras)
     print("Computed AUC of the ROC:")
     print(model_auc_score)
 
@@ -135,11 +147,8 @@ def main(input_file_name, target_name, n_cut=25):
     print("")
     print("Classification report")
 
-
-
     print(classification_report(np.array(target_threshold_predictions, dtype="int32"),
                                 np.array(f5_test_target[:, target_index], dtype="int32")))
-
 
 
 if __name__ == "__main__":
@@ -152,35 +161,12 @@ if __name__ == "__main__":
 
     arg_parse_obj.add_argument("-n", "--n-cut-off", dest="n_cut_off", default="25")
 
+    #TODO: Add command line switches
+
+    # def main(input_file_name, target_name, n_cut=25, prediction_threshold=0.5, epochs=10, batch_size=100,
+    #          learning_rate=1e-3, learning_rate_decay=1e-4):
+
+
     arg_obj = arg_parse_obj.parse_args()
 
     main(arg_obj.hdf5_file_name, arg_obj.target, int(arg_obj.n_cut_off))
-
-    # main("Y:\\healthfacts\\ts\\processed_ohdsi_sequences.hdf5", "Acute renal failure syndrome")
-
-    # Code attic
-
-    # print(target_labels)
-    # target_index = target_labels.index("static_condition_condition_concept_name|Anxiety disorder")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Essential hypertension")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Type 2 diabetes mellitus without complication")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Respiratory failure")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Heart failure")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Chronic kidney disease")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Hyperkalemia")
-    # target_index = target_labels.index("static_condition_condition_concept_name|Sepsis")
-
-
-    # configure sgd with gradient norm clipping
-    # from tensorflow.keras.optimizers import SGD
-    # opt = SGD(lr=0.01, momentum=0.9, clipnorm=1.0)
-
-    # model.add(SimpleRNN(128, activation="tanh", return_sequences=True))
-    # model.add(Dropout(0.10))
-    #
-    # model.add(Dense(64, activation="sigmoid"))
-    # #model.add(Dropout(0.10))
-
-    # model.add(LSTM(128, activation="tanh", input_shape=(100,182)))
-
-    # fpr_keras, tpr_keras, thresholds_keras = roc_curve(np.array(f5_test_target[:, target_index],dtype="int32"), y_pred_keras)
