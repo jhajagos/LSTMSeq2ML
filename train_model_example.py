a9d86a0882f776ee3dd9157a54e98c584663734f
janos <risk.limits@gmail.com>
2019-11-24 13:27:32 -0500

    Runttime tracking

--- train_model_example.py
+++ train_model_example.py
@@ -2,12 +2,14 @@ import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout, Masking, SimpleRNN
+from sklearn.metrics import roc_curve, roc_auc_score, f1_score, classification_report, average_precision_score
 import numpy as np
 import h5py
 import pprint
 import argparse
 import datetime
 import os
+import json
 
 
 """
@@ -19,9 +21,9 @@ The CuDNNLSTM is much faster but it does not support Masking
 
 
 def main(input_file_name, target_name, output_directory="./", n_cut=25, prediction_threshold=0.5, epochs=10, batch_size=100,
-         learning_rate=1e-3, learning_rate_decay=1e-4):
+         learning_rate=1e-3, learning_rate_decay=1e-4, target_prefix="static_condition_condition_concept_name|"):
 
-    target_index_name = "static_condition_condition_concept_name|" + target_name
+    target_index_name = target_prefix + target_name
 
     f5 = h5py.File(input_file_name)
 
@@ -33,6 +35,9 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
 
     print("Dimensions of training set:")
     print(f5_train.shape)
+
+    training_size_n, features_n = f5_train.shape
+
     print("Dimension of full target:")
     print(f5_target.shape)
 
@@ -41,7 +46,9 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
 
     # Get a count of the most frequent labels
     sum_targets = np.sum(f5_target[...], axis=0).tolist()
-    sum_target_with_labels = [(target_labels[i].split("|")[-1], sum_targets[i], sum_targets[i] / f5_train.shape[0]) for i in range(len(target_labels))]
+    sum_target_with_labels = [(target_labels[i].split("|")[-1], sum_targets[i], sum_targets[i] / f5_train.shape[0])
+                              for i in range(len(target_labels))]
+
     sum_target_with_labels.sort(key=lambda x: -1 * x[1])
 
     # Let us generate a list of most frequent features to target for prediction
@@ -57,6 +64,8 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
 
     print("Dimensions of test set:")
     print(f5_test.shape)
+    testing_size_n, _ = f5_test.shape
+
     print("Dimensions of target:")
     print(f5_test_target.shape)
 
@@ -68,6 +77,8 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
     f5_test_array = f5_test[...]
     f5_test_array[np.isnan(f5_test_array)] = 0
 
+    start_date_time = datetime.date.utcnow()
+
     #TODO: Make models plugin
     model = Sequential()
     model.add(Masking(mask_value=0.0, input_shape=f5_train_array.shape[1:]))
@@ -77,7 +88,6 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
     model.add(Dropout(0.2))
     model.add(Dense(128, activation="relu"))
     model.add(Dropout(0.2))
-
     model.add(Dense(1, activation="sigmoid"))
 
     opt = tf.keras.optimizers.Adam(lr=learning_rate, decay=learning_rate_decay)
@@ -89,7 +99,7 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
     es_callback = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2, restore_best_weights=True)
     mc_callback = ModelCheckpoint(best_model_file_name, monitor='val_loss', mode='min', verbose=1, save_best_only=True)
 
-    # fit model
+    # Fit the model to the data
 
     # This can be refactored as explicit casts are not needed
     model.fit(np.array(f5_train_array, dtype="float32"), np.array(f5_target[:, target_index], dtype="int32"), epochs,
@@ -99,13 +109,45 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
 
     model.load_weights(best_model_file_name)
 
-    time_stamp = datetime.date.utcnow().strftime("%Y%m%d_%H%M")
+    end_date_time = datetime.datetime.utcnow()
+    end_label_time_stamp = end_date_time.strftime("%Y%m%d_%H%M")
+    start_time_stamp = start_date_time.strftime("%Y-%m-%d %H:%M:%S")
+    end_time_stamp = end_date_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    coefficients_filename = target_name_label + "_" + time_stamp + "_coeffs.hdf5"
+    time_lapse = end_time_stamp - start_time_stamp
+
+    coefficients_filename = target_name_label + "_" + end_label_time_stamp + "_coeffs.hdf5"
     model.save(os.path.joins(output_directory, coefficients_filename))
 
     # Make a probability prediction
     y_pred_keras = model.predict_proba(np.array(f5_test[...], dtype="float32")).ravel()
+
+    # Diagnostics
+    results_dict = {"meta": {}}
+    python_program_run = os.path.abspath(__file__)
+    meta_dict = results_dict["meta"]
+    meta_dict["python_program_run"] = python_program_run
+    meta_dict["start_date_time_utc"] = start_time_stamp
+    meta_dict["end_date_time_utc"] = end_time_stamp
+    meta_dict["time_lapse"] = str(time_lapse)
+
+    results_dict["model"] = {}
+    model_dict = results_dict["model"]
+
+    model_dict["learning_rate"] = learning_rate
+    model_dict["learning_rate_decay"] = learning_rate_decay
+    model_dict["batch_size"] = batch_size
+    model_dict["epochs"] = epochs
+
+    results_dict["target"] = {}
+    results_dict["target"]["target_name"] = target_name
+    results_dict["target"]["target_name_label"] = target_name_label
+
+    results_dict["data"] = {}
+    results_dict["data"]["input_file_name"] = os.path.abspath(input_file_name)
+
+    results_dict["training_size_n"] = training_size_n
+    results_dict["features_n"] = features_n
 
     print("Diagnostics of predictions")
 
@@ -121,34 +163,62 @@ def main(input_file_name, target_name, output_directory="./", n_cut=25, predicti
     print("Probability of first %s predictions from the model:" % show_first_n)
     print(y_pred_keras[0:show_first_n])
 
+    total_positive_cases_test_set = np.sum(f5_test_target[:, target_index])
     print("Total positive cases in test set:")
-    print(np.sum(f5_test_target[:, target_index]))
+    print(total_positive_cases_test_set)
 
+    results_dict["test"] = {}
+    test_dict = results_dict["test"]
+    test_dict["total_positive_cases_test_set"] = total_positive_cases_test_set
+
+    total_positive_cases_training_set = np.sum(f5_target[:, target_index])
+    results_dict["data"]["total_positive_cases_training_set"] = total_positive_cases_training_set
+
+    sum_predicted_test_set = np.sum(target_threshold_predictions)
     print("Total predicted positive cases in test set:")
-    print(np.sum(target_threshold_predictions))
+    print(sum_predicted_test_set)
+    test_dict["sum_predicted_test_set"] = sum_predicted_test_set
 
+    sum_of_probabilities_test_set = np.sum(y_pred_keras)
     print("Total sum of probabilities")
-    print(np.sum(y_pred_keras))
-
-    from sklearn.metrics import roc_curve, roc_auc_score, f1_score, classification_report, average_precision_score
+    print(sum_of_probabilities_test_set)
+    test_dict["sum_of_probabilities_test_set"] = sum_of_probabilities_test_set
 
     model_auc_score = roc_auc_score(np.array(f5_test_target[:, target_index], dtype="int32"), y_pred_keras)
     print("Computed AUC of the ROC:")
     print(model_auc_score)
+    test_dict["model_auc_score"] = model_auc_score
 
     f1 = f1_score(np.array(target_threshold_predictions, dtype="int32"), np.array(f5_test_target[:, target_index], dtype="int32"))
     print("F1 score:")
     print(f1)
+    test_dict["f1_score"] = f1
 
-    avg_prec_rec = average_precision_score(np.array(target_threshold_predictions, dtype="int32"), np.array(f5_test_target[:, target_index], dtype="int32"))
+    avg_prec_rec = average_precision_score(np.array(target_threshold_predictions, dtype="int32"),
+                                           np.array(f5_test_target[:, target_index], dtype="int32"))
     print("Average precision score:")
     print(avg_prec_rec)
+    test_dict["average_precision_recall"] = avg_prec_rec
 
     print("")
     print("Classification report")
 
-    print(classification_report(np.array(target_threshold_predictions, dtype="int32"),
-                                np.array(f5_test_target[:, target_index], dtype="int32")))
+    cr_report = classification_report(np.array(target_threshold_predictions, dtype="int32"),
+                                np.array(f5_test_target[:, target_index], dtype="int32"))
+
+    print(cr_report)
+    test_dict["classification_report"] = cr_report
+
+    prediction_results_file_name = os.path.join(output_directory, "predicted_" + target_name_label + "_" + end_label_time_stamp)
+
+    with h5py.File(prediction_results_file_name, "w") as f5w:
+        ds = f5w["/"].create_dataset(shape=target_threshold_predictions.shape, dtype=target_threshold_predictions.dtype)
+        ds[...] = target_threshold_predictions[...]
+
+    results_dict_file_name = os.path.join(output_directory, target_name_label + "_" + end_label_time_stamp)
+
+    with open(results_dict_file_name) as fw:
+        json.dump(results_dict, fw, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
@@ -161,12 +231,23 @@ if __name__ == "__main__":
 
     arg_parse_obj.add_argument("-n", "--n-cut-off", dest="n_cut_off", default="25")
 
-    #TODO: Add command line switches
+    arg_parse_obj.add_argument("-o", "--output-directory", dest="output_directory", default="./")
 
-    # def main(input_file_name, target_name, n_cut=25, prediction_threshold=0.5, epochs=10, batch_size=100,
-    #          learning_rate=1e-3, learning_rate_decay=1e-4):
+    arg_parse_obj.add_argument("-p", "--prediction-threshold", dest="prediction_threshold", default="0.5")
+
+    arg_parse_obj.add_argument("-e", "--epochs", dest="epochs", default="10")
+
+    arg_parse_obj.add_argument("-b", "--batch-size", dest="batch_size", default="50")
+
+    arg_parse_obj.add_argument("-l", "--learning-rate", dest="learning_rate", default="1e-3")
+
+    arg_parse_obj.add_argument("-d", "--learning-rate-decay", dest="learning_rate_decay", default="1e-4")
 
 
     arg_obj = arg_parse_obj.parse_args()
 
-    main(arg_obj.hdf5_file_name, arg_obj.target, int(arg_obj.n_cut_off))
+    main(arg_obj.hdf5_file_name, arg_obj.target, n_cut=int(arg_obj.n_cut_off), output_directory=arg_obj.output_directory,
+         prediction_threshold=float(arg_obj.prediction_threshold), epochs=int(arg_obj.epochs),
+         batch_size=int(arg_obj.batch_size), learning_rate=float(arg_obj.learning_rate),
+         learning_rate_decay=float(arg_obj.learning_rate_decay)
+         )
